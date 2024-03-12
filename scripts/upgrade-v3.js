ac6c9e2b2fb1201c1a8a004113d5518a64049f45
Jesse B. Miller <jesse@jessebmiller.com>
2024-03-12 09:54:07 -0500

    deployed to optimism

--- scripts/upgrade-v3.js
+++ scripts/upgrade-v3.js
@@ -6,23 +6,26 @@ const { ethers, upgrades } = hre;
 
 var nftAddress;
 switch (hre.network.name) {
-  case 'mainnet':
-    nftAddress = process.env.MAINNET_NFT_ADDRESS;
-    break;
-  case 'goerli':
-    nftAddress = process.env.GOERLI_NFT_ADDRESS;
-    break;
-  case 'sepolia':
-    nftAddress = process.env.SEPOLIA_NFT_ADDRESS;
-    break;
-  case 'hardhat':
-    nftAddress = process.env.HARDHAT_NFT_ADDRESS;
-    break;
-  case 'localhost':
-    nftAddress = process.env.LOCALHOST_NFT_ADDRESS;
-    break;
-  default:
-    throw new Error('Unsupported network');
+case 'mainnet':
+  nftAddress = process.env.MAINNET_NFT_ADDRESS;
+  break;
+case 'goerli':
+  nftAddress = process.env.GOERLI_NFT_ADDRESS;
+  break;
+case 'sepolia':
+  nftAddress = process.env.SEPOLIA_NFT_ADDRESS;
+  break;
+case 'optimism':
+    nftAddress = process.env.OPT_NFT_ADDRESS;
+  break;
+case 'hardhat':
+  nftAddress = process.env.HARDHAT_NFT_ADDRESS;
+  break;
+case 'localhost':
+  nftAddress = process.env.LOCALHOST_NFT_ADDRESS;
+  break;
+default:
+  throw new Error('Unsupported network. Add', hre.network.name, "to upgrade-v3.js");
 }
 
 if (!nftAddress) {
